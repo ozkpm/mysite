bb4ad548160bb12e4a787dd7ccc74a3170ba3ad6
System Administrator <root@yunoMac-mini.local>
2023-07-03 00:57:24 +0900

    add: first commit

---
+++ src/.vuepress/components/ArticleList.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+      <h3> 記事一覧 </h3>
+        <ul>
+            <li v-for="(item, index) in articles">
+                <router-link v-bind:to="getLink(index)">
+                {{ item.title }}: {{ item.date }}
+                </router-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        articles: [
+            {
+                "title": "自作HPを作った",
+                "date": "2023-07-02"
+            }
+        ]
+      }
+    },
+    methods: {
+      getLink(value) {
+        value = value + 1
+        return '/articles/' + value + '.html'
+
+      }
+    }
+  }
+</script>
---
+++ src/.vuepress/components/IllustGallery.vue
@@ -0,0 +1,47 @@
+<template>
+    <v-app id="app">
+    <div>
+      <h3> 書いた絵 </h3>
+      <v-row>
+          <div v-for="(image, index) in lists" :key="index">
+            <v-col>
+            <v-img :src="image.url"
+              max-height="250"
+              max-width="250"
+              @click="setDialogUrl(index)"
+              >
+            </v-img>
+          </v-col>
+          </div>
+      </v-row>
+      <v-dialog v-model="dialog" width="1000" contained>
+        <v-img :src="dialogUrl" :max-height="800" :max-width="1000" 
+        contain ></v-img>
+      </v-dialog>
+    </div>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    // components: { vGallery },
+    data() {
+      return {
+        lists: [
+          {title: '桃華', url:'/momoka.jpeg'},
+          {title: '千冬', url:'/chifuyu.jpeg'}
+        ],
+        dialog: false,
+        dialogUrl: '',
+      }
+    },
+    computed: {
+    },
+    methods: {
+      setDialogUrl(index) {
+        this.dialogUrl = this.lists[index].url
+        this.dialog = true
+      },
+    }
+  }
+</script>
